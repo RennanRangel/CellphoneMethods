e3e468cf410a99df05a5f4b1c333dac84affaf67
RennanRangel <rennano965@gmail.com>
2025-04-02 14:00:21 -0300

    Add files via upload

---
+++ CellPhoneMethods/src/Main.java
@@ -0,0 +1,21 @@
+import capa.Capa;
+
+class loja {
+
+
+    public static void main(String[] args) {
+
+
+        Capa capa = new Capa("Verde", "Inox", 250);
+        Celular celular = new Celular("Iphone", "Cinza", "S25", 1500,capa);
+
+
+        celular.calcularTotal();
+
+
+    }
+
+
+
+
+}
